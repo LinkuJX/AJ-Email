43e78da6dd533c5dd2da7fb48b66602f1b3de145
Linku <kylelin78@gmail.com>
2017-04-16 20:04:24 -0400

    Fixed form-control input and inline forms

--- AJ-Email/index.php
+++ AJ-Email/index.php
@@ -84,106 +84,35 @@ include('head.html');
             </div>
             <div class="panel-body">
                 <div class="form-group" id="received">
-                    <div>
-                        <div class="checkbox-inline">
-                            <input type="checkbox" data-toggle="toggle" data-on="DOB" data-off="DOB" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="ADD" data-off="ADD" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="PayPal" data-off="PayPal" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="Card" data-off="Card" data-size="mini">
-                            <label for="card-card-number" class="checkbox-label">Card #:</label>
-                            <input type="text" class="form-control">
-                            <input type="checkbox" data-toggle="toggle" data-on="POP" data-off="POP" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="3P" data-off="3P" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="Recent" data-off="Recent" data-size="mini">
-                            <label for="pop-3p-recent-date" class="checkbox-label">Date:</label>
-                            <input type="date" class="form-control">
-                        </div>
+                    <div class="artificial-row"> <!-- For CSS use -->
+                        <input type="checkbox" data-toggle="toggle" data-on="DOB" data-off="DOB" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="ADD" data-off="ADD" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="PayPal" data-off="PayPal" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="Card" data-off="Card" data-size="mini">
+                        <label for="card-card-number" class="checkbox-label">Card #: </label>
+                        <input type="text" class="form-inline">
+                        <input type="checkbox" data-toggle="toggle" data-on="POP" data-off="POP" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="3P" data-off="3P" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="Recent" data-off="Recent" data-size="mini">
+                        <label for="pop-3p-recent-date" class="checkbox-label">Date: </label>
+                        <input type="date" class="form-inline">
                     </div>
-                    <div>
-                        <div class="checkbox-inline">
-                            <input type="checkbox" data-toggle="toggle" data-on="Name Change" data-off="Name Change" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="ADD Change" data-off="ADD Change" data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="PP Trans." data-off="PP Trans." data-size="mini">
-                            <input type="checkbox" data-toggle="toggle" data-on="3P Card" data-off="3P Card" data-size="mini">
-                            <label for="threep-card-number" class="checkbox-label">3P Card #:</label>
-                            <input type="text" class="form-control">
-                            <input type="checkbox" data-toggle="toggle" data-on="Payment" data-off="Payment" data-size="mini">
-                            <label for="payment-amount" class="checkbox-label">Amount:</label>
-                            <input type="number" class="form-control">
-                        </div>
+                    <div class="artificial-row">
+                        <input type="checkbox" data-toggle="toggle" data-on="Name Change" data-off="Name Change" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="ADD Change" data-off="ADD Change" data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="PP Trans." data-off="PP Trans." data-size="mini">
+                        <input type="checkbox" data-toggle="toggle" data-on="3P Card" data-off="3P Card" data-size="mini">
+                        <label for="threep-card-number" class="checkbox-label">3P Card #: </label>
+                        <input type="text" class="form-inline">
+                        <input type="checkbox" data-toggle="toggle" data-on="Payment" data-off="Payment" data-size="mini">
+                        <label for="payment-amount" class="checkbox-label">Amount: </label>
+                        <input type="number" class="form-inline">
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </form>
-
-<script id="outputTemplate" type="text/x-jsrender">
-##[Countdown]({{:CountdownUrl}})  
-
-Teams
----
-
-{{:Standings}}
-
---- 
-
-Streams
----
-
-{{:Streams}}
-{{:StreamsYt1}}
-
-{{:StreamsYt2}}
-
----
-
-Hosts
----
-
-{{:Hosts}}
-
----
-
-Casters
----
-
-{{:Casters}}
-
----
-
-Schedule
----
-
-{{:Schedule}}
-
-* All times are APPROXIMATE and the schedule should be used as a general guideline.
-
-* All matches will be played on patch [{{:Patch}}]({{:PatchLinkNumber}}).
-
----
-
-{{:Footer}}
-</script>
-
-<script id="streamsTemplate" type="text/x-jsrender">
-* [{{:Name}}]({{:Link}})
-</script>
-
-<script id="castersTemplate" type="text/x-jsrender">
-* {{:Name}} [Twitter]({{:Twitter}})
-</script>
-
-<script id="footerStuffTemplate" type="text/x-jsrender">
-{{:Title}}
----
-{{for Content}}
-{{:}}
-{{/for}}
-
----
-
-</script>
 
 <?php
 
