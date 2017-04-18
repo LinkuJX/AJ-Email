9ef126caa93f0d410e2ad90c2acf0aa2d065d4ed
Linku <kylelin78@gmail.com>
2017-04-18 12:23:12 -0400

    Finished adding inputs

--- AJ-Email/js/main.js
+++ AJ-Email/js/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
+    
     console.log("READY");
+    
+    // Brand Selection Buttons
     $("#brand button.btn-brand").click(function() {
         $("#brand button.btn-brand").removeClass('active');
         $(this).addClass('active');
     });
+    
+    // Collapsible Panels
+    $(document).on('click', '.panel-heading span.clickable', function(e){
+        var $this = $(this);
+        if(!$this.hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideUp();
+            $this.addClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        } else {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $this.removeClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        }
+    });
+    
+    $(".inputgroup input").inputmask();
 });
